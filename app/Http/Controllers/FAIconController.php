fb75e8a5be83fca350232df208e5a00fe8b70fe0
Ady G [TAFE] <adrian.gould@nmtafe.wa.edu.au>
2020-11-20 09:52:45 +0800

    Added FontAwesome Free, initialised Models/Migrations/etc
    NOTE: DO NOT UPGRADE TO TailwindCSS v2 as it breaks the FontAwesome SASS importing

---
+++ app/Http/Controllers/FAIconController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FAIcon;
+use Illuminate\Http\Request;
+
+class FAIconController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FAIcon  $fAIcon
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FAIcon $fAIcon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FAIcon  $fAIcon
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FAIcon $fAIcon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FAIcon  $fAIcon
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FAIcon $fAIcon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FAIcon  $fAIcon
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FAIcon $fAIcon)
+    {
+        //
+    }
+}
